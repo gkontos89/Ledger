85bbe28397494dceacbd77e2c9041609ac72ad09
gkontos89 <gkontos89@gmail.com>
2018-01-04 23:00:15 -0600

    updates to periods

--- src/InvestmentPeriodManager.py
+++ src/InvestmentPeriodManager.py
@@ -5,15 +5,16 @@ This module contains the class for expresses time periods when calculation Retur
 from datetime import datetime
 
 
-class RoiManager(object):
+class InvestmentPeriodManager(object):
     """! This class manages ROI data and calculations for the entire application and all transactions
     """
     def __init__(self):
-        """! Constructor fot the RoiManager class
+        """! Constructor for the InvestmentPeriodManager class
         """
-        pass
+        ## instance of an InvestmentAll object
+        self.investmentAll = InvestmentAll('All')
 
-    def runRoiAnalysis(self, dateString, buyFor, sellFor):
+    def processTransaction(self, txId, dateData, buyFor, sellFor):
         """! This function kicks off ROI calculations based on a transaction received
 
         Args:
@@ -21,31 +22,43 @@ class RoiManager(object):
             buyFor - int - amount the asset was purchased for in $'s
             sellFor - int - amount the asset was sold for in $'s
         """
-        pass
+        dateData = datetime.strptime(dateData, '%m/%d/%Y')
+        self.investmentAll.processTransaction(txId, dateData, buyFor, sellFor)
 
-    def getAllTimeRoi(self):
+    def getAllTimeInvestments(self):
         """! This function will return all-time transaction data with relevant ROI calculations
-        """
-        pass
 
-    def getRoiByYear(self, year):
+        Return:
+            tuple of (buyForTotal, sellForTotal, roi) for all time
+        """
+        return self.investmentAll.getTransactionIds(), self.investmentAll.buyForTotal, self.investmentAll.sellForTotal, self.investmentAll.roi
+
+    def getInvestmentsByYear(self, year):
         """! This function will return transaction data with relevant ROI calculations for the specified year
 
         Args:
             year - string - the year to return transaction data from
-        """
-        pass
 
-    def getRoiByMonth(self, year, month):
+        Return:
+            tuple of (buyForTotal, sellForTotal, roi) for the given year
+        """
+        investmentYear = self.investmentAll.investmentYears[year]
+        return investmentYear.getTransactionIds(), investmentYear.buyForTotal, investmentYear.sellForTotal, investmentYear.roi
+
+    def getInvestmentsByMonth(self, year, month):
         """! This function will return transaction data with relevant ROI calculations for the specified year
 
         Args:
             year - string - the year to return transaction data from
             month - string - returns ledger transactions with ROI numbers for the given month
-        """
-        pass
 
-class RoiPeriod(object):
+        Return:
+            tuple of (buyForTotal, sellForTotal, roi) for a given month
+        """
+        investmentMonth = self.investmentAll.investmentYears[year].investmentMonths[month]
+        return investmentMonth.getTransactionIds(), investmentMonth.buyForTotal, investmentMonth.sellForTotal, investmentMonth.roi
+
+class InvestmentPeriod(object):
     """!  This is the base class for calculating ROI in a given time period
     """
     def __init__(self, title, *args, **kwargs):
@@ -63,7 +76,7 @@ class RoiPeriod(object):
         ## return on investment in %
         self.roi = 0
 
-    def processRoi(self, dateData, buyFor, sellFor):
+    def processTransaction(self, txId, dateData, buyFor, sellFor):
         """! This function receives and stores buying and selling information for an asset and performs the
              ROI calculation for the period is represents
 
@@ -74,17 +87,30 @@ class RoiPeriod(object):
         """
         self.buyForTotal += buyFor
         self.sellForTotal += sellFor
-        self.roi = (self.sellForTotal - self.buyForTotal) / self.buyForTotal
+        if self.buyForTotal > 0:
+            self.roi = (self.sellForTotal - self.buyForTotal) / self.buyForTotal
 
-class RoiMonth(RoiPeriod):
+    def getTransactionIds(self):
+        pass
+
+class InvestmentMonth(InvestmentPeriod):
     """! This class represents the smallest division of a time period for calculating ROI
     """
     def __init__(self, *args, **kwargs):
         """! Constructor for the RoiMonth class
         """
         super().__init__(*args, **kwargs)
+        ## transactions id's that occurred during this period
+        self.txIds = []
 
-class RoiYear(RoiPeriod):
+    def processTransaction(self, txId, dateData, buyFor, sellFor):
+        super().processTransaction(txId, dateData, buyFor, sellFor)
+        self.txIds.append(txId)
+
+    def getTransactionIds(self):
+        return self.txIds
+
+class InvestmentYear(InvestmentPeriod):
     """! This class represents a year of ROI calculations
     """
     def __init__(self, *args, **kwargs):
@@ -92,13 +118,13 @@ class RoiYear(RoiPeriod):
         """
         super().__init__(*args, **kwargs)
         ## Dictionary of {'monthId', RoiMonth object}
-        self.roiMonths = {}
+        self.investmentMonths = {}
         for i in range(0, 12):
             dateObj = datetime.strptime(str(i+1), '%m')
             title = dateObj.strftime('%b')
-            self.roiMonths[title] = RoiMonth(title)
+            self.investmentMonths[title] = InvestmentMonth(title)
 
-    def processRoi(self, dateData, buyFor, sellFor):
+    def processTransaction(self, txId, dateData, buyFor, sellFor):
         """! Override of the base class implementation. Calculates ROI on itself using the base class implementation
              then pass down the transaction data to the sub roi periods
 
@@ -108,24 +134,31 @@ class RoiYear(RoiPeriod):
             sellFor - int - amount the asset was sold for in $'s
         """
         # Calculate ROI on itself
-        super().processRoi(dateData, buyFor, sellFor)
+        super().processTransaction(txId, dateData, buyFor, sellFor)
 
-        # Call processRoi on specific month
+        # Call processTransaction on specific month
         month = dateData.strftime('%b')
-        self.roiMonths[month].processRoi(dateData, buyFor, sellFor)
+        self.investmentMonths[month].processTransaction(txId, dateData, buyFor, sellFor)
 
-class RoiAll(RoiPeriod):
+    def getTransactionIds(self):
+        txIds = []
+        for title, investmentMonth in self.investmentMonths.items():
+            txIds += investmentMonth.getTransactionIds()
+
+        return txIds
+
+class InvestmentAll(InvestmentPeriod):
     """! This class represents the all time ROI calculations and is the entry point for processing all ROI
          calculations.  This handles the parsing of date string and converting as necessary
     """
     def __init__(self, *args, **kwargs):
-        """! Constructor for the RoiAll class.
+        """! Constructor for the InvestmentAll class.
         """
         super().__init__(*args, **kwargs)
         ## dictionary of {'yearId', RoiYear object}
-        self.roiYears = {}
+        self.investmentYears = {}
 
-    def processRoi(self, dateData, buyFor, sellFor):
+    def processTransaction(self, txId, dateData, buyFor, sellFor):
         """! Override of the base class implementation. Calculates ROI on itself using the base class implementation
              then pass down the transaction data to the sub roi periods
 
@@ -135,14 +168,21 @@ class RoiAll(RoiPeriod):
             sellFor - int - amount the asset was sold for in $'s
         """
         # Calculate ROI on itself
-        super().processRoi(dateData, buyFor, sellFor)
+        super().processTransaction(txId, dateData, buyFor, sellFor)
 
-        # Call processRoi on relevant roi time period
+        # Call processTransaction on relevant roi time period
         year = dateData.strftime('%Y')
-        if year not in self.roiYears:
-            self.roiYears[year] = RoiYear(year)
+        if year not in self.investmentYears:
+            self.investmentYears[year] = InvestmentYear(year)
 
-        self.roiYears[year].processRoi(dateData, buyFor, sellFor)
+        self.investmentYears[year].processTransaction(txId, dateData, buyFor, sellFor)
+
+    def getTransactionIds(self):
+        txIds = []
+        for title, investmentYear in self.investmentYears.items():
+            txIds += investmentYear.getTransactionIds()
+
+        return txIds
 
 
 
--- src/Ledger.py
+++ src/Ledger.py
@@ -1,13 +1,25 @@
 """
 """
 
-from enum import Enum
+from src.InvestmentPeriodManager import InvestmentPeriodManager
+from datetime import datetime
 
+class Transaction(object):
+    def __init__(self, id, date, order, assetId, quantity, amount):
+        self.id = id
+        self.date = date
+        self.order = order
+        self.assetId = assetId
+        self.quantity = quantity
+        self.amount = amount
 
+        self.txFinePrint = str(self.id) + ' -- ' + self.date + ' -- ' + self.order + ' -- ' + self.assetId + ' -- '\
+                           + self.quantity + ' -- ' + self.amount
 
-class Order(Enum):
-    BUY = 0
-    SELL = 1
+class AssetPurchase(object):
+    def __init__(self, quantity, price):
+        self.quantity = quantity
+        self.price = price
 
 class Ledger(object):
     """! This class handles all the transaction data and computes ROI's based on
@@ -28,20 +40,56 @@ class Ledger(object):
         """! Initialization function of the Ledger class.  This program, for now, is meant to be self contained
              so all transactions information and revelant holdings are stored as python serialized objects and loaded as such
         """
-        pass
+        ## Each transactions increments the counter.  TODO: Look for better way of generating unique tx id's
+        self.txIdCounter = -1
+        ## Dictionary of {txId : Transaction()} pairs
+        self.rawLedger = {}
+        ## Dictionary of {assetId : AssetPurchase()} for assets purchased.  Used for calculating ROI when a SELL occurs
+        self.fifoAssetDict = {}
+        ## Instance of the investment period manager
+        self.investmentPeriodManager = InvestmentPeriodManager()
 
-    def enterTransaction(self, date, order, assetId, quantity, amount):
+    def enterTransaction(self, date, order, assetId, quantity, price):
         """! This function is used to record a transaction in the ledger, which in turn calculates ROI for all
              appropriate periods.  Creates a unique transaction id.
 
         Args:
-            data - string - date of the transaction - must be in MM/DD/YYYY format
-            order - Order - type of order of the transaction
+            date - string - date of the transaction - must be in MM/DD/YYYY format
+            order - string - type of order of the transaction - BUY, SELL
             assetId - string - id of the asset of the transaction
             quantity - int - number of shares bought/sold
-            amount - int - amount in dollars
+            price - int - price in dollars of the transactions
         """
-        pass
+        self.txIdCounter += 1 #TODO: Look for better way of generating unique tx id's
+        tx = Transaction(self.txIdCounter, date, order, assetId, quantity, price)
+        self.rawLedger[self.txIdCounter] = tx
+        if assetId not in self.fifoAssetDict:
+            self.fifoAssetDict[assetId] = []
+
+        # buyFor and sellFor are only relevant when a SELL takes place.  When this occurs the buyFor price is
+        # calculated using FIFO methods, and the buyFor/sellFor is passed into the InvestmentPeriodManager for processing
+        # and storage.
+        if order == 'BUY':
+            self.fifoAssetDict[assetId].append(AssetPurchase(quantity, price))
+            buyFor = 0
+            sellFor = 0
+        else:
+            # In the case of an asset being sold, the buyFor data is taken from the fifo asset dict
+            buyFor = 0
+            sellFor = price
+            while quantity > 0:
+                if quantity >= self.fifoAssetDict[assetId][0].quantity:
+                    assetPurchase = self.fifoAssetDict[assetId].pop(0)
+                    buyFor += assetPurchase.price
+                    quantity -= assetPurchase.quantity
+                else:
+                    pricePerUnit = self.fifoAssetDict[assetId][0].price / self.fifoAssetDict[assetId][0].quantity
+                    self.fifoAssetDict[assetId][0].quantity -= quantity
+                    self.fifoAssetDict[assetId][0].price = pricePerUnit * self.fifoAssetDict[assetId][0].quantity
+                    buyFor += (pricePerUnit * quantity)
+                    quantity = 0
+
+        self.investmentPeriodManager.processTransaction(tx.id, tx.date, buyFor, sellFor)
 
     def deleteTransaction(self, transactionId):
         """! This function will delete a transaction based specified by the unique ID
@@ -57,7 +105,7 @@ class Ledger(object):
         pass
 
     def getTransactionsByYear(self, year):
-        """! This function will return transaction data for a givcen year
+        """! This function will return transaction data for a given year
 
         Args:
             year - string - the year to return transaction data from
