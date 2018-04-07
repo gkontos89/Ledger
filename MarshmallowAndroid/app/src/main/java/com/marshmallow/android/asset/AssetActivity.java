640dc1b67ec9d995f9397bffb3e7bf92e85da08e
George Kontos <gkontos89@gmail.com>
2018-04-07 00:54:35 -0500

    added start of milestone files

---
+++ MarshmallowAndroid/app/src/main/java/com/marshmallow/android/asset/AssetActivity.java
@@ -0,0 +1,15 @@
+package com.marshmallow.android.asset;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.marshmallow.android.R;
+
+public class AssetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_asset);
+    }
+}
