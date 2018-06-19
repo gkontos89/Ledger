6d737385b23369ac57cee859793ad91a194d0b41
George Kontos <gkontos89@gmail.com>
2018-06-18 19:28:33 -0500

    updated guis

--- MarshmallowAndroid/app/src/main/java/com/marshmallow/android/ui/AccountSelectionActivity.java
+++ MarshmallowAndroid/app/src/main/java/com/marshmallow/android/ui/AccountSelectionActivity.java
@@ -10,6 +10,6 @@ public class AccountSelectionActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_account_selection);
+        setContentView(R.layout.activity_create_account_selection);
     }
 }
--- MarshmallowAndroid/app/src/main/java/com/marshmallow/android/ui/SummaryActivity.java
+++ MarshmallowAndroid/app/src/main/java/com/marshmallow/android/ui/SummaryActivity.java
@@ -2,18 +2,14 @@ package com.marshmallow.android.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 import com.marshmallow.android.R;
 
-public class TransactionActivity extends AppCompatActivity {
+public class SummaryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_transaction);
-    }
-
-    public void onRadioButtonClicked(View view) {
+        setContentView(R.layout.activity_summary);
     }
 }
